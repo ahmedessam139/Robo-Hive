ff1012cd23a18a478433684584f4dd40547b7c5b
Hoda Elnaghy <hhesham2582000@gmail.com>
2023-03-03 19:45:29 +0200

    EW

--- RPA_Slayer/Activities/Custom Acitivities/Processes Control/Processes Control/close_app.cs
+++ RPA_Slayer/Activities/Custom Acitivities/Processes Control/Processes Control/close_app.cs
@@ -3,20 +3,41 @@ using System.Activities;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Diagnostics;
 
 namespace Processes_Control
 {
+    [System.ComponentModel.Designer(typeof(close_app_desginer), typeof(System.ComponentModel.Design.IDesigner))]
+
     public sealed class close_app : CodeActivity
     {
-        // Define an activity input argument of type string
-        public InArgument<string> Text { get; set; }
+        // Define an activity input argument of type int
+        public InArgument<int> ProcessId { get; set; }
 
-        // If your activity returns a value, derive from CodeActivity<TResult>
-        // and return the value from the Execute method.
         protected override void Execute(CodeActivityContext context)
         {
-            // Obtain the runtime value of the Text input argument
-            string text = context.GetValue(this.Text);
+            // Obtain the runtime value of the ProcessId input argument
+            int processId = context.GetValue(this.ProcessId);
+
+            try
+            {
+                // Get the process by its ID
+                Process process = Process.GetProcessById(processId);
+
+                // Close the process's main window
+                process.CloseMainWindow();
+
+                // Wait for the process to exit
+                process.WaitForExit();
+
+                // Dispose of the process
+                process.Dispose();
+            }
+            catch (ArgumentException ex)
+            {
+                // The process with the specified ID was not found
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
