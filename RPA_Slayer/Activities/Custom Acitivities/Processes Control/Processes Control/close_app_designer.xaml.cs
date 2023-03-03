1ee6ad356d6e5fe9bd90ca8c4a3cc1a8fed12d69
Ahmed_Essam <ahmedessam139ae@gmail.com>
2023-03-03 18:44:20 +0200

    //

---
+++ RPA_Slayer/Activities/Custom Acitivities/Processes Control/Processes Control/close_app_designer.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Processes_Control
+{
+    // Interaction logic for close_app_designer.xaml
+    public partial class close_app_designer
+    {
+        public close_app_designer()
+        {
+            InitializeComponent();
+        }
+    }
+}
