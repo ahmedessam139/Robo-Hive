be9b92d33664c9f10ea9d23e561fadd94917b1ee
Ahmed_Essam <ahmedessam139ae@gmail.com>
2023-06-24 05:10:31 +0300

    prepare screen for recording

--- RPA_Slayer/Helpers/recording.cs
+++ RPA_Slayer/Helpers/recording.cs
@@ -4,6 +4,12 @@ using System.Threading;
 using System.Windows.Automation;
 using System.IO;
 using System.Windows.Input;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Drawing;
+using System.Linq;
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
 
 namespace RPA_Slayer.Helpers
 {
@@ -13,21 +19,72 @@ namespace RPA_Slayer.Helpers
         private List<AutomationElement> _recordedActions = new List<AutomationElement>();
         private List<string> _elementNames = new List<string>();
 
+        private const int WS_EX_TOPMOST = 0x00000008;
+        private const int WS_EX_NOACTIVATE = 0x08000000;
+        private const int WS_EX_TRANSPARENT = 0x00000020;
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool SetLayeredWindowAttributes(IntPtr hWnd, uint crKey, byte bAlpha, uint dwFlags);
+
+        private const int GWL_EXSTYLE = -20;
+        private const int WS_EX_LAYERED = 0x80000;
+
+        private Form overlayForm;
+        private Label overlayLabel;
+
         public void StartRecord(string workflowFilePath)
         {
             // Find the root element of the desktop
             _rootElement = AutomationElement.RootElement;
 
-            // Subscribe to UI automation events 
+            // Create and show the overlay form
+            CreateOverlayForm();
+
+            // Subscribe to UI automation events
             Automation.AddAutomationEventHandler(InvokePattern.InvokedEvent, _rootElement, TreeScope.Descendants, OnInvoked);
             Automation.AddAutomationFocusChangedEventHandler(OnFocusChanged);
 
             Console.WriteLine("Recording started...");
         }
 
-        private void recordKeyPressing(object sender, KeyEventArgs e)
+        private void CreateOverlayForm()
         {
-            Console.WriteLine("Key pressed: {0}", e.Key);
+            overlayForm = new Form();
+            overlayForm.FormBorderStyle = FormBorderStyle.None;
+            overlayForm.ShowInTaskbar = false;
+            overlayForm.BackColor = Color.Black;
+            overlayForm.TransparencyKey = Color.Black;
+            overlayForm.TopMost = true;
+            overlayForm.Size = new Size(300, 75); // Set the desired size of the overlay form
+            overlayForm.StartPosition = FormStartPosition.Manual;
+            overlayForm.Location = new Point(Screen.PrimaryScreen.WorkingArea.Right - overlayForm.Width, Screen.PrimaryScreen.WorkingArea.Bottom - overlayForm.Height); // Position the form in the bottom right corner
+
+            overlayLabel = new Label();
+            overlayLabel.Text = "Recording......\nPress F8 to stop";
+            overlayLabel.ForeColor = Color.White;
+            overlayLabel.Font = new Font("Arial", 14, FontStyle.Bold);
+            overlayLabel.AutoSize = true;
+            overlayLabel.Location = new Point(10, 10);
+
+            overlayForm.Controls.Add(overlayLabel);
+            overlayForm.Show();
+
+            SetWindowLong(overlayForm.Handle, GWL_EXSTYLE, GetWindowLong(overlayForm.Handle, GWL_EXSTYLE) | WS_EX_LAYERED | WS_EX_NOACTIVATE);
+            SetLayeredWindowAttributes(overlayForm.Handle, 0, 128, 0x2);
+        }
+
+        private void DestroyOverlayForm()
+        {
+            overlayForm.Close();
+            overlayForm.Dispose();
+            overlayForm = null;
+            overlayLabel = null;
         }
 
         private void OnFocusChanged(object sender, AutomationFocusChangedEventArgs e)
@@ -40,7 +97,6 @@ namespace RPA_Slayer.Helpers
                 {
                     Console.WriteLine("Focused element AutomationId------>: {0}", focusedElement.Current.AutomationId);
                     Console.WriteLine("Focused element Name------>: {0}", focusedElement.Current.Name);
-                    
                 }
             }
             catch (Exception ex)
@@ -48,8 +104,6 @@ namespace RPA_Slayer.Helpers
                 Console.WriteLine("Error occurred in OnFocusChanged: {0}", ex.Message);
             }
         }
-
-
 
         private void OnInvoked(object sender, AutomationEventArgs e)
         {
@@ -68,21 +122,23 @@ namespace RPA_Slayer.Helpers
             }
         }
 
-       
-
         public string StopRecord(string filePath)
         {
-            //remove All event handlers
+            // Remove all event handlers
             Automation.RemoveAllEventHandlers();
+
+            // Destroy the overlay form
+            DestroyOverlayForm();
+
             // Read the content of the file
             string content = File.ReadAllText(filePath);
             // Split the content into lines
             string[] lines = content.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
             // Compute the index of the line before the last 2 lines
-            int insertionIndex = Math.Max(0, lines.Length - 2);            
+            int insertionIndex = Math.Max(0, lines.Length - 2);
             // Insert the elements before the line at the insertion index
             lines[insertionIndex] = string.Join("" + Environment.NewLine, _elementNames) + Environment.NewLine + lines[insertionIndex];
-            //remove all elemnts (_elementNames)
+            // Remove all elements (_elementNames)
             _elementNames.Clear();
             // Join the lines back into a single string
             content = string.Join(Environment.NewLine, lines);
@@ -92,6 +148,5 @@ namespace RPA_Slayer.Helpers
             // Return the updated content
             return content;
         }
-
     }
 }
