494ea0f355b8ddbeaffbdc75ccbe253221cbbc4a
Ahmed_Essam <ahmedessam139ae@gmail.com>
2023-06-26 18:08:50 +0300

    Add Keyboard listener to Recorging Engine

---
+++ RPA_Slayer/Activities/Custom Acitivities/UI Automation/UI_Automation/UI_Automation/FocusElement.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Activities;
+using System.Windows.Automation;
+
+namespace UI_Automation
+{
+    public sealed class FocusElement : CodeActivity
+    {
+        [RequiredArgument]
+        public InArgument<string> ElementName { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            try
+            {
+                // Get the automation element by name
+                string elementName = ElementName.Get(context);
+                AutomationElement element = AutomationElement.RootElement.FindFirst(
+                    TreeScope.Descendants, new PropertyCondition(AutomationElement.NameProperty, elementName));
+
+                if (element == null)
+                {
+                    throw new ArgumentException("Element with name '{elementName}' not found.");
+                }
+
+                // Set focus on the element
+                element.SetFocus();
+            }
+            catch (Exception ex)
+            {
+                // Handle the exception or rethrow it if needed
+                // You can log the error, display a message, or perform any other necessary action
+                Console.WriteLine("Error occurred in FocusElement: {ex.Message}");
+            }
+        }
+    }
+}
--- RPA_Slayer/Helpers/recording.cs
+++ RPA_Slayer/Helpers/recording.cs
@@ -19,6 +19,9 @@ namespace RPA_Slayer.Helpers
         private AutomationElement _rootElement;
         private List<AutomationElement> _recordedActions = new List<AutomationElement>();
         private List<string> _elementNames = new List<string>();
+        private bool _isKeyboardRecordingActive = false;
+        private LowLevelKeyboardHook _keyboardHook;
+        private List<string> word = new List<string>();
 
         private const int WS_EX_TOPMOST = 0x00000008;
         private const int WS_EX_NOACTIVATE = 0x08000000;
@@ -54,10 +57,106 @@ namespace RPA_Slayer.Helpers
             CreateOverlayForm();
 
             // Subscribe to UI automation events
-            //Automation.AddAutomationEventHandler(InvokePattern.InvokedEvent, _rootElement, TreeScope.Descendants, OnInvoked);
-            Automation.AddAutomationFocusChangedEventHandler(OnFocusChanged);
+            Automation.AddAutomationEventHandler(InvokePattern.InvokedEvent, _rootElement, TreeScope.Descendants, OnInvoked);
+            //Automation.AddAutomationFocusChangedEventHandler(OnFocusChanged);
+
+            // Enable global keyboard recording
+            _isKeyboardRecordingActive = true;
+            _keyboardHook = new LowLevelKeyboardHook();
+            _keyboardHook.KeyDownEvent += OnKeyDown;
+            _keyboardHook.Install();
 
             Console.WriteLine("Recording started...");
+        }
+
+        private void OnInvoked(object sender, AutomationEventArgs e)
+        {
+            try
+            {
+                AutomationElement element = sender as AutomationElement;
+                Console.WriteLine("Invoked element AutomationId------>: {0}", element.Current.AutomationId);
+
+                if (word.Count > 0)
+                {
+                    AddWordToList();
+                    word.Clear();
+                }
+
+
+                if (true)
+                {
+                    _recordedActions.Add(element);
+                    // Save the element name in XML format to the list
+                    string xmlElementName = string.Format("<u:InvokeElement ElementName=\"{0}\" />", element.Current.Name);
+                    _elementNames.Add(xmlElementName);
+                    // Print the accumulated list of element names
+                    Console.WriteLine(string.Join(Environment.NewLine, _elementNames));
+                    Console.WriteLine(_elementNames);
+                    // CLICK ON ELEMENT
+                    Thread.Sleep(200);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error occurred in OnInvoked: {0}", ex.Message);
+            }
+        }
+
+        private void OnFocusChanged(object sender, AutomationFocusChangedEventArgs e)
+        {
+            try
+            {
+                if (word.Count > 0)
+                {
+                    AddWordToList();
+                    word.Clear();
+                }
+                AutomationElement focusedElement = sender as AutomationElement;
+                string elementName = focusedElement.Current.Name;
+                Console.WriteLine("Focus changed to: " + elementName);
+                Thread.Sleep(500);
+
+
+                if (focusedElement != null)
+                {
+                    _recordedActions.Add(focusedElement);
+                    // Save the element name in XML format to the list
+                    string xmlElementName = string.Format("<u:FocusElement ElementName=\"{0}\" />", elementName);
+                    _elementNames.Add(xmlElementName);
+                    // Print the accumulated list of element names
+                    Console.WriteLine(string.Join(Environment.NewLine, _elementNames));
+                    Console.WriteLine(_elementNames);
+                    // CLICK ON ELEMENT
+                    Thread.Sleep(200);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error occurred in OnFocusChanged: {0}", ex.Message);
+            }
+        }
+
+        private void OnKeyDown(object sender, LowLevelKeyboardEventArgs e)
+        {
+            if (_isKeyboardRecordingActive)
+            {
+                string key = e.Key.ToString();
+
+                if (key.Length == 1 && char.IsLetterOrDigit(key[0]))
+                {
+                    word.Add(key);
+                    string fullWord = string.Join("", word);
+                    Console.WriteLine("Current word: " + fullWord);
+                }
+            }
+        }
+
+        private void AddWordToList()
+        {
+            string fullWord = string.Join("", word);
+            string formattedWord = $"<i:KeyboardControl Text=\"{fullWord}\" />";
+            _elementNames.Add(formattedWord);
+
         }
 
         private void MinimizeAllWindows()
@@ -66,7 +165,7 @@ namespace RPA_Slayer.Helpers
             foreach (Process process in processes)
             {
                 IntPtr hWnd = process.MainWindowHandle;
-                ShowWindow(hWnd, 2); // Minimize window
+                ShowWindow(hWnd, 2); // Minimize window sfawertva adf
             }
         }
 
@@ -78,7 +177,7 @@ namespace RPA_Slayer.Helpers
             overlayForm.BackColor = Color.Black;
             overlayForm.TransparencyKey = Color.Black;
             overlayForm.TopMost = true;
-            overlayForm.Size = new Size(400, 100); // Set the desired size of the overlay form
+            overlayForm.Size = new Size(400, 100); // Set the desired size of the overlay formss
             overlayForm.StartPosition = FormStartPosition.Manual;
             overlayForm.Location = new Point(Screen.PrimaryScreen.WorkingArea.Right - overlayForm.Width, Screen.PrimaryScreen.WorkingArea.Bottom - overlayForm.Height); // Position the form in the bottom right corner
 
@@ -104,7 +203,6 @@ namespace RPA_Slayer.Helpers
             SetLayeredWindowAttributes(overlayForm.Handle, 0, 128, 0x2);
         }
 
-
         private void DestroyOverlayForm()
         {
             overlayForm.Close();
@@ -113,55 +211,38 @@ namespace RPA_Slayer.Helpers
             overlayLabel = null;
         }
 
-        private void OnFocusChanged(object sender, AutomationFocusChangedEventArgs e)
-        {
-            try
-            {
-                AutomationElement focusedElement = sender as AutomationElement;
-                string elementName = focusedElement.Current.Name;
-                Console.WriteLine("Focus changed to: " + elementName);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Error occurred in OnFocusChanged: {0}", ex.Message);
-            }
-        }
-
-        private void OnInvoked(object sender, AutomationEventArgs e)
-        {
-            AutomationElement element = sender as AutomationElement;
-            Console.WriteLine("Invoked element AutomationId------>: {0}", element.Current.AutomationId);
-            if (element != null)
-            {
-                _recordedActions.Add(element);
-                // Save the element name in XML format to the list
-                string xmlElementName = string.Format("<u:InvokeElement ElementName=\"{0}\" />", element.Current.Name);
-                _elementNames.Add(xmlElementName);
-                // Print the accumulated list of element names
-                Console.WriteLine(string.Join(Environment.NewLine, _elementNames));
-                Console.WriteLine(_elementNames);
-                // CLICK ON ELEMENT
-                Thread.Sleep(200);
-            }
-        }
-
         public string StopRecord(string filePath)
         {
+
+            if (word.Count > 0)
+            {
+                AddWordToList();
+                word.Clear();
+            }
+            // Disable global keyboard recording
+            _isKeyboardRecordingActive = false;
+            _keyboardHook.Uninstall();
+
             // Remove all event handlers
             Automation.RemoveAllEventHandlers();
 
             // Destroy the overlay form
             DestroyOverlayForm();
 
-
             // Read the content of the file
             string content = File.ReadAllText(filePath);
             // Split the content into lines
-            string[] lines = content.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
-            // Compute the index of the line before the last 2 lines
-            int insertionIndex = Math.Max(0, lines.Length - 2);
+            List<string> lines = content.Split(new[] { Environment.NewLine }, StringSplitOptions.None).ToList();
+
+            // Compute the index of the line where you want to insert the new lines
+            int insertionIndex = Math.Max(0, Math.Min(lines.Count, 3));
+
+            // Append the new lines at the desired index
+            //lines.InsertRange(insertionIndex, new[] { " xmlns:u=\"clr-namespace:UI_Automation;assembly=UI_Automation\"", " xmlns:i=\"clr-namespace:IO_Modules;assembly=IO-Modules\"" });
+            
+            int insertionIndex1 = Math.Max(0, lines.Count - 2);
             // Insert the elements before the line at the insertion index
-            lines[insertionIndex] = string.Join("" + Environment.NewLine, _elementNames) + Environment.NewLine + lines[insertionIndex];
+            lines[insertionIndex1] = string.Join("" + Environment.NewLine, _elementNames) + Environment.NewLine + lines[insertionIndex1];
             // Remove all elements (_elementNames)
             _elementNames.Clear();
             // Join the lines back into a single string
@@ -173,6 +254,82 @@ namespace RPA_Slayer.Helpers
             return content;
         }
 
-       
+
+        // Low-level keyboard hook class
+        public class LowLevelKeyboardHook
+    {
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+
+        private readonly LowLevelKeyboardProc _proc;
+        private IntPtr _hookID = IntPtr.Zero;
+
+        public event EventHandler<LowLevelKeyboardEventArgs> KeyDownEvent;
+
+        public LowLevelKeyboardHook()
+        {
+            _proc = HookCallback;
+        }
+
+        public void Install()
+        {
+            _hookID = SetHook(_proc);
+        }
+
+        public void Uninstall()
+        {
+            UnhookWindowsHookEx(_hookID);
+        }
+
+        private IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                KeyDownEvent?.Invoke(this, new LowLevelKeyboardEventArgs((Keys)vkCode));
+            }
+
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        #region DLL Imports
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        #endregion
     }
+
+    // Event arguments for low-level keyboard eventsfdsa
+    public class LowLevelKeyboardEventArgs : EventArgs
+    {
+        public Keys Key { get; }
+
+        public LowLevelKeyboardEventArgs(Keys key)
+        {
+            Key = key;
+        }
+    }
+}
 }
